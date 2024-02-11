230c315e41ed4fcd658bcaebdb399c624f6252cd
Filip CIca <72666124+FC122@users.noreply.github.com>
2024-02-11 12:36:59 +0100

    Update test

--- AirConditionerTest.js
+++ AirConditionerTest.js
@@ -1,31 +1,31 @@
-const assert = require('assert');
 const { describe, it } = require("mocha");
+const { assert } = require("chai");
 const AirConditioner = require("./AirConditioner.js");
 
 describe("CoffeMachine", () => {
 
     it('Check that machine can be turned on', () => {
         let ac = new AirConditioner(true);
-        assert.strictEqual(ac.toggle(), ac.powerStatus)
-    })
+        assert.strictEqual(ac.toggle(), ac.powerStatus);
+    });
 
     it('Check that machine can be turned off', () => {
         let ac = new AirConditioner(false);
-        assert.strictEqual(ac.toggle(), ac.powerStatus)
-    })
+        assert.strictEqual(ac.toggle(), ac.powerStatus);
+    });
 
     it('Check that user can set temp', () => {
         let ac = new AirConditioner(false);
-        assert.strictEqual(ac.setTemp(25), 25)
-    })
+        assert.strictEqual(ac.setTemp(25), 25);
+    });
 
     it('Check that user cant set temp too high', () => {
         let ac = new AirConditioner(false);
-        assert.strictEqual(ac.setTemp(101), 20)
-    })
+        assert.strictEqual(ac.setTemp(101), 20);
+    });
 
     it('Check that user cant set temp too low', () => {
         let ac = new AirConditioner(true);
-        assert.strictEqual(ac.setTemp(0), 20)
-    })
+        assert.strictEqual(ac.setTemp(0), 20);
+    });
 });
